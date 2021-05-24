62e4c6599b72f626cdc41bea6241ae7985db30f4
Vadim <vadiman158@gmail.com>
2021-05-24 18:08:35 +0700

    added css styles for the welcome page and registration form

--- src/main/java/ru/project/controller/RegistrationController.java
+++ src/main/java/ru/project/controller/RegistrationController.java
@@ -30,7 +30,7 @@ public class RegistrationController {
         User userFromDb = userRepo.findByUsername(user.getUsername());
 
         if(userFromDb != null) {
-            model.put("message", "User exists!");
+            model.put("message", "Пользователь уже существует!");
             return "registration";
         }
 
